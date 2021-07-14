85df7dba8825a267694467ae009a17ab9b689694
mabutou <mabutou@gmail.com>
2021-07-14 11:32:41 +0800

    update theme

--- uptheme.sh
+++ uptheme.sh
@@ -1,5 +1,6 @@
 cd /Users/mabutou/code/blog_vercel/nobelium
 git pull
+rm -rf /Users/mabutou/code/blog_vercel/blog/*
 cp -r /Users/mabutou/code/blog_vercel/nobelium/* /Users/mabutou/code/blog_vercel/blog/
 cp -r /Users/mabutou/Documents/code/blog_vercel/* /Users/mabutou/code/blog_vercel/blog/
 cd /Users/mabutou/code/blog_vercel/blog

a06836653e2d4cc4afc12db49ed2040310224610
mabutou <mabutou@gmail.com>
2021-07-14 15:01:03 +0800

    update theme

--- components/Footer.js
+++ components/Footer.js
@@ -19,7 +19,9 @@ const Footer = ({ fullWidth }) => {
           <Vercel />
         </div>
       </div>
+      <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"heihei@tuta.io",utcoffset:"8"}))};sessionStorage.setItem("_swa","1");</script>
     </div>
+    
   )
 }
 
